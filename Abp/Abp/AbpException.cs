13a0df5b7aa3a2e2461b05953bdd89e54f1954e6
xins Bin <q25a25q@live.com>
2015-11-06 14:48:50 +0800

    添加 Base Exception

---
+++ Abp/Abp/AbpException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Abp
+{
+    /// <summary>
+    /// 对于那些基本的异常类型是由ABP系统ABP特定的异常抛出。
+    /// </summary>
+    [Serializable]
+    public class AbpException : Exception
+    {
+        /// <summary>
+        /// Creates a new <see cref="AbpException"/> object.
+        /// </summary>
+        public AbpException()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AbpException"/> object.
+        /// </summary>
+        public AbpException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AbpException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public AbpException(string message)
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AbpException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Inner exception</param>
+        public AbpException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
